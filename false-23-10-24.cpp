03d2e157d07fb94f479aa4338bd2c26ef08aa569
QSDEL <137593187+QSDEL@users.noreply.github.com>
2023-10-24 08:08:54 +0800

    Add files via upload

---
+++ false-23-10-24.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+//int main() {
+//    int a = 1, b = 3, c = 2;
+//    while (a < b < c)
+//    {
+//        ++a;
+//        --b;
+//        --c;
+//    }
+//    cout << a << " " << b << " " << c << "\n";
+//    return 0;
+//}
+
+//  a < b < c 
+//判断顺序：从左到右
+
+//switch语句后的控制表达式只能是short、char、int、long整数类型和枚举类型，不能是float，double和boolean类型
